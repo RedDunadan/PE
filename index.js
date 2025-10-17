ab1611e4fb256e1c11c7bca538e4d562ef60b43a
RedDunadan <devrhedd@gmail.com>
2025-10-17 12:45:17 -0300

    agregando mensajes de instrucciones en caso de poner un comando invalido.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const [, , method, resource, ...args] = process.argv;
 
 const BASE_URL = "https://fakestoreapi.com";
+
+(async () => {
+  try {
+    if (!method || !resource) {
+      console.log("❗Uso correcto:");
+      console.log("npm run start GET products");
+      console.log("npm run start GET products/<id>");
+      console.log("npm run start POST products <title> <price> <category>");
+      console.log("npm run start DELETE products/<id>");
+      return;
+    }
